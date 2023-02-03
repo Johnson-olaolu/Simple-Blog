bc6b4c175d52927d14572e0ba855f3443734ea46
Olaolu Johnson <johnsonolaolu@gmail.com>
2023-02-03 08:35:44 +0100

    updated

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -16,27 +16,27 @@ export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  async create(@Body() createPostDto: CreatePostDto) {
-    return await this.postService.create(createPostDto);
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.create(createPostDto);
   }
 
   @Get()
-  async findAll() {
-    return await this.postService.findAll();
+  findAll() {
+    return this.postService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.postService.findOne(+id);
+  findOne(@Param('id') id: string) {
+    return this.postService.findOne(+id);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return await this.postService.update(+id, updatePostDto);
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postService.update(+id, updatePostDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.postService.remove(+id);
+  remove(@Param('id') id: string) {
+    return this.postService.remove(+id);
   }
 }
